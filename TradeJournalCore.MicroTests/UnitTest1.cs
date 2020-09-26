0dcbee31462a6dd0be5903cac4e3ea9427ae67d7
Andrew <andrewbiggins@live.co.uk>
2020-09-26 14:39:03 +0100

    Add project files.

---
+++ TradeJournalCore.MicroTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TradeJournalCore.MicroTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
