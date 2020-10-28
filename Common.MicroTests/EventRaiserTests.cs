2a7e83e0837618e71744a0936273858f4b46f16a
Andrew <andrewbiggins@live.co.uk>
2020-10-28 10:59:14 +0000

    Added event raiser unit tests

---
+++ Common.MicroTests/EventRaiserTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NSubstitute;
+using System.ComponentModel;
+
+namespace Common.MicroTests
+{
+    public sealed class EventRaiserTests
+    {
+        [Gwt("Given a property changed event has a handler",
+            "when the event is raised",
+            "the handler is invoked")]
+        public void T0()
+        {
+            // Arrange
+            var catcher = Catcher.PropertyChanged;
+            PropertyChangedEvent += catcher.Catch;
+            const string propertyName = nameof(propertyName);
+
+            // Act
+            PropertyChangedEvent.Raise(this, propertyName);
+
+            // Assert
+            catcher.CaughtPropertyChanged(this, propertyName);
+        }
+
+        [Gwt("Given an event has a handler",
+            "when the event is raised",
+            "the handler is invoked")]
+        public void T1()
+        {
+            // Arrange
+            var catcher = Catcher.Simple;
+            Event += catcher.Catch;
+
+            // Act
+            Event.Raise(this);
+
+            // Assert
+            catcher.CaughtEmpty(this);
+        }
+
+        private event EventHandler? Event;
+        private event PropertyChangedEventHandler? PropertyChangedEvent;
+    }
+}
